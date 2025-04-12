a10d446ae1847cf477f82b5ccd74b2d3224e71e2
Shomy 🍂 <61943525+shomykohai@users.noreply.github.com>
2025-04-12 18:52:23 +0200

    Remove example package from package list as well

--- default.nix
+++ default.nix
@@ -6,6 +6,5 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
   kde-plasma-control-hub = pkgs.callPackage ./pkgs/kde-plasma-control-hub { };
 }
