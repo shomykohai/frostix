8630e5115144fec4d6a7d406ea15bd638c9e0e75
Shomy 🍂 <61943525+shomykohai@users.noreply.github.com>
2025-11-03 15:12:07 +0100

    Ensure sub derivations are kept

--- flake.nix
+++ flake.nix
@@ -1,11 +1,22 @@
 {
   description = "Frostix Nix repository";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
+  outputs = { self, nixpkgs }: let
     forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+
+    filterDerivationsRec = attrs:
+      builtins.foldl' (acc: key:
+        let val = attrs.${key}; in
+        if nixpkgs.lib.isDerivation val then
+          acc // { ${key} = val; }
+        else if builtins.isAttrs val then
+          let nested = filterDerivationsRec val; in
+          if builtins.length (builtins.attrNames nested) > 0 then
+            acc // { ${key} = nested; }
+          else acc
+        else acc
+      ) {} (builtins.attrNames attrs);
+
   in {
     legacyPackages = forAllSystems (system:
       import ./default.nix {
@@ -14,6 +25,8 @@
           config.allowUnfree = true;
         };
       });
-    packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
+
+    packages = forAllSystems (system:
+      filterDerivationsRec self.legacyPackages.${system});
   };
 }
