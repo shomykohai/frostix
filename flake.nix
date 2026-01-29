9ceae460534c980ba1e58bc31d5b717ec1baecdd
Shomy 🍂 <61943525+shomykohai@users.noreply.github.com>
2026-01-29 21:10:17 +0100

    Add hydra jobs

--- flake.nix
+++ flake.nix
@@ -1,22 +1,28 @@
 {
   description = "Frostix Nix repository";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  outputs = { self, nixpkgs }: let
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
     forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
 
     filterDerivationsRec = attrs:
-      builtins.foldl' (acc: key:
-        let val = attrs.${key}; in
-        if nixpkgs.lib.isDerivation val then
-          acc // { ${key} = val; }
-        else if builtins.isAttrs val then
-          let nested = filterDerivationsRec val; in
-          if builtins.length (builtins.attrNames nested) > 0 then
-            acc // { ${key} = nested; }
+      builtins.foldl' (
+        acc: key: let
+          val = attrs.${key};
+        in
+          if nixpkgs.lib.isDerivation val
+          then acc // {${key} = val;}
+          else if builtins.isAttrs val
+          then let
+            nested = filterDerivationsRec val;
+          in
+            if builtins.length (builtins.attrNames nested) > 0
+            then acc // {${key} = nested;}
+            else acc
           else acc
-        else acc
       ) {} (builtins.attrNames attrs);
-
   in {
     legacyPackages = forAllSystems (system:
       import ./default.nix {
@@ -26,7 +32,10 @@
         };
       });
 
-    packages = forAllSystems (system:
-      filterDerivationsRec self.legacyPackages.${system});
+    packages =
+      forAllSystems (system:
+        filterDerivationsRec self.legacyPackages.${system});
+
+    hydraJobs = self.packages;
   };
 }
